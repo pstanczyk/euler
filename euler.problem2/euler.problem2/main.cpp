06c97e526d9a63540c31348b1ed610f9e592f8f3
Piotr Stanczyk <pstanczyk@piotrs-macbook.local>
2019-01-06 11:41:28 -0800

    setup for problem2

---
+++ euler.problem2/euler.problem2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  euler.problem2
+//
+//  Created by Piotr Stanczyk on 1/6/19.
+//  Copyright © 2019 Piotr Stanczyk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
